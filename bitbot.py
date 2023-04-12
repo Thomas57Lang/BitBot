0e79f783b3ef197fa8b84cedf9b1388dd1ec04aa
Thomas Langston <thomas57langston@gmail.com>
2023-04-12 19:01:20 -0400

    Implemented function to send and recieve chat messages.

--- bitbot.py
+++ bitbot.py
@@ -1,16 +1,42 @@
 import os
 import discord
 from dotenv import load_dotenv
+import openai
 
 load_dotenv()
-
 discord_token = os.getenv('DISCORD')
-gpt_token = os.getenv('OPEN_AI')
+openai.api_key = os.getenv('OPEN_AI')
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+currmodel = 'gpt-3.5-turbo'
 
-client = discord.Client(intents=discord.Intents.default())
+async def contact_gpt(content):
+    #Pass message content to openai API
+    completion = openai.ChatCompletion.create(
+        model=currmodel, 
+        messages=[
+            {"role": "system", "content": "You are a chatbot running in a discord server."},
+            {"role": "user", "content": content}
+        ]
+    )
+    return completion.choices[0].message.content
+
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+    guild = discord.utils.get(client.guilds)
+    print(f'Bitbot is connected to {guild}')
+    
+@client.event
+async def on_message(message):
+    #print(f'Message detected - {message.content}')
+    if message.author == client.user:
+        return
+    if message.content.startswith('bb!'):
+        print('User invoked bitbot command')
+        response = await contact_gpt(message.content)
+        await message.channel.send(response)
 
 client.run(discord_token)
