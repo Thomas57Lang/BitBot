25b68497e7109d6d8a73be74a3c9010cf2010c5a
Thomas Langston <thomas57langston@gmail.com>
2023-04-13 21:55:32 -0400

    Implemented memory access to bb's input messages.

--- bitbot.py
+++ bitbot.py
@@ -12,6 +12,38 @@ intents.message_content = True
 client = discord.Client(intents=intents)
 currmodel = 'gpt-3.5-turbo'
 
+def create_log():
+    title = datetime.now().isoformat()
+    path = f'Logs/{title}'
+    log = open(path, 'w')
+    log.write('{"role": "system", "content": "You are a chatbot running in a discord server. Your name is bb which is short for BitBot."}\n')
+    log.close()
+    return path
+
+def assistant_append_log(content, path):
+    log = open(path, 'a')
+    log.write('{"role": "assistant", "content": ' + content + '}\n')
+    log.close()
+
+def user_append_log(content, path):
+    log = open(path, 'a')
+    log.write('{"role": "user", "content": ' + content + '}\n')
+    log.close()
+    return
+
+def user_message(content):
+    message = {
+        "role":"user",
+        "content":content
+    }
+    return message
+
+def assistant_message(content):
+    message = {
+        "role":"assistant",
+        "content":content
+    }
+    return message
 
 async def bb_command(content):
     if (len(content) < 2):
@@ -23,18 +55,23 @@ async def bb_command(content):
     else:
         return await contact_gpt(content)
 
-
 async def contact_gpt(content):
+    #Append content to log
+    usermsg = user_message(content)
+    user_append_log(content, logpath)
+    messages.append(usermsg)
     #Pass message content to openai API
     completion = openai.ChatCompletion.create(
-        model=currmodel, 
-        messages=[
-            {"role": "system", "content": "You are a chatbot running in a discord server. Your name is bb which is short for BitBot. You are invoked by using the 'bb!' command."},
-            {"role": "user", "content": content}
-        ]
+        model=currmodel,
+        messages=messages
     )
+    #Append assistant message to log
+    assistantmsg = assistant_message(completion.choices[0].message.content)
+    assistant_append_log(completion.choices[0].message.content, logpath)
+    messages.append(assistantmsg)
+    totaltk = completion.usage.total_tokens
+    print(f'Total tokens used: {totaltk}')
     return completion.choices[0].message.content
-
 
 async def generate_image(user_prompt):
     #Pass prompt content to openai API
@@ -50,6 +87,16 @@ async def on_ready():
     print(f'{client.user} has connected to Discord!')
     guild = discord.utils.get(client.guilds)
     print(f'Bitbot is connected to {guild}')
+    global logpath
+    global messages
+    messages = [
+        {
+            "role": "system", 
+            "content": "You are a chatbot running in a discord server. Your name is bb which is short for BitBot."
+        }
+    ]
+    logpath = create_log()
+    print(f'New chat log created: {logpath}')
     
 @client.event
 async def on_message(message):
@@ -61,3 +108,5 @@ async def on_message(message):
         await message.channel.send(response)
 
 client.run(discord_token)
+#logpath = create_log()
+#append_log('Test', logpath)
